6cd2ab17ff1bbb10f6d791f886f1448e22db8be8
tung2389 <36613569+tung2389@users.noreply.github.com>
2019-02-03 15:27:44 +0700

    fix routing when user has logged in

---
+++ precache-manifest.d30379ec1c23579d6cf5a630c80ce9e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6cdb27f3bed58ea9bec",
+    "url": "/authentication_with_session/static/js/runtime~main.f6cdb27f.js"
+  },
+  {
+    "revision": "fe8eb171ae9bf935183a",
+    "url": "/authentication_with_session/static/js/main.fe8eb171.chunk.js"
+  },
+  {
+    "revision": "8e7ccbe53e0a9a213dac",
+    "url": "/authentication_with_session/static/js/1.8e7ccbe5.chunk.js"
+  },
+  {
+    "revision": "fe8eb171ae9bf935183a",
+    "url": "/authentication_with_session/static/css/main.5283d191.chunk.css"
+  },
+  {
+    "revision": "8f5ebd93ef34327b23d172aa13b9f2e6",
+    "url": "/authentication_with_session/index.html"
+  }
+];
